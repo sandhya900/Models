6456f02bf15eaa46ea35d99151f269e0b8404dae
Admin <Admin@DESKTOP-24271K1>
2020-07-12 15:40:22 +0530

    Add project files.

---
+++ UserRegistrationViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+
+namespace Innova_TRIAL.ViewModels.RegisterViewModel
+{
+    public class UserRegistrationViewModel
+    {
+       
+        public string FirstName { get; set; }
+
+      
+        public string LastName { get; set; }
+
+
+        public string Email { get; set; }
+
+     
+        public string Password { get; set; }
+
+
+        public string ConfirmPassword { get; set; }
+
+        public bool IsVerified { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool SendNotification { get; set; }
+
+
+
+    }
+}
